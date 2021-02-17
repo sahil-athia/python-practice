1dcbd085623457699300c82b71d31a8b956e4501
Sahil Athia <sahilathia918@gmail.com>
2021-02-17 14:48:29 -0500

    basic use of comparisons

---
+++ comparison_op.py
@@ -0,0 +1,18 @@
+a = 3
+b = 4
+print a == b
+print a != b
+print a > b
+print a < b
+print "Hello" == "hello"
+print "2" == 2
+print 2.0 == 2
+print 2 >= 2
+
+# logical operators (and, or, not)
+
+print(1 < 2 and 2 < 3)
+print("h" == "h" and 2 == 2)
+print(False or True) # this will print true 
+print(True or False) # this will print true also
+print not(1 == 1) # prints the opposite boolean
