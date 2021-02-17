252b583cbc80943f4f2be359ea6d17080bd5df55
Sahil Athia <sahilathia918@gmail.com>
2021-02-17 15:26:33 -0500

    use if elif and else

---
+++ if_statements.py
@@ -0,0 +1,22 @@
+# uses if elif and else
+if True:
+  print "its true"
+
+if not False: 
+  print "its False"
+
+if 2 < 1:
+  print "hi"
+elif 2 == 2:
+  print "this should print"
+else:
+  print "hello"
+
+location = "store"
+
+if location == "store":
+  print "at the store"
+elif location == "mall":
+  print "at the mall"
+else:
+  print "where are we"
