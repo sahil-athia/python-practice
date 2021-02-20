75462133f02b4dee9916d25719f68cf08e8b9386
Sahil Athia <sahilathia918@gmail.com>
2021-02-20 14:02:18 -0500

    use keyword arguments

--- methods_and_functions/args_kwargs.py
+++ methods_and_functions/args_kwargs.py
@@ -11,7 +11,20 @@ print my_func(40, 60)
 
 def new_func(*args):
   # this will cause all of the parameters passed in to default into a tuple
-  # the word also doesnt have to be "args", we can say *stuff
+  # the word also doesnt have to be "args", we can say *stuff (but use args as convention)
   return sum(args) * 0.05
 
 print new_func(40, 60, 100, 90, 10)
+
+# kwargs will build a dictionary
+
+def other_func(**kwargs):
+  print kwargs
+  if 'fruit' in kwargs:
+    print("my fruit of choice is: {}".format(kwargs["fruit"]))
+  else:
+    print("no fruit located")
+
+other_func(fruit="apple", veggie="lettuce")
+
+
