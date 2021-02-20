95d5fa0adcf7e008f914cc97abdd01895d58d3a2
Sahil Athia <sahilathia918@gmail.com>
2021-02-20 14:04:51 -0500

    use both args and kwargs in one function

--- methods_and_functions/args_kwargs.py
+++ methods_and_functions/args_kwargs.py
@@ -27,4 +27,9 @@ def other_func(**kwargs):
 
 other_func(fruit="apple", veggie="lettuce")
 
+def both_args_func(*args, **kwargs):
+  print args
+  print kwargs
+  print "I would like {} {}".format(args[0], kwargs['food'])
 
+both_args_func(10, 20, 30, food = 'orange', fruit = "apple")
