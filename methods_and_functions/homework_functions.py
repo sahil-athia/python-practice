c3897f666e4066348dfa1ba3ca23ef56e08ec2d0
Sahil Athia <sahilathia918@gmail.com>
2021-02-21 20:30:42 -0500

    check if value is in range

--- methods_and_functions/homework_functions.py
+++ methods_and_functions/homework_functions.py
@@ -2,11 +2,18 @@ import math
 import string
 
 ## calculate sphere volume 
-
 def volume(radius):
   return (4.0 / 3.0) * ((radius ** 3) * (math.pi))
 
 print volume(2)
+
+
+## check if first num is in range of the next 2
+def in_range(num, low, high):
+  range_list = range(low, high)
+  return num in range_list
+
+print in_range(11,1,10)
 
 ## check upper and lower clase letters
 def upper_lower(text):
@@ -42,8 +49,8 @@ def palindrome(text):
 print palindrome('helleh')
 print palindrome('hellcvtequvybijeh')
 
-## determine text is a pangram
 
+## determine text is a pangram
 def is_pangram(str1, alphabet = string.ascii_lowercase):
   ## remove spaces in text
   new_str = str1.replace(" ", "")
@@ -56,7 +63,7 @@ def is_pangram(str1, alphabet = string.ascii_lowercase):
 
   ## make the list a string
   final_str = "".join(uniqe_str)
-  
+
   return final_str in alphabet
 
 print is_pangram("The quick brown fox jumps over the lazy dog")
