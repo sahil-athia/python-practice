4deb68ab69e377e98d6a716682b8ffab36f2877b
Sahil Athia <sahilathia918@gmail.com>
2021-02-21 20:10:30 -0500

    check upper and lower case letters

--- methods_and_functions/homework_functions.py
+++ methods_and_functions/homework_functions.py
@@ -1,6 +1,21 @@
 import math
 
+## calculate sphere volume 
+
 def volume(radius):
   return (4.0 / 3.0) * ((radius ** 3) * (math.pi))
 
 print volume(2)
+
+## check upper and lower clase letters
+def upper_lower(text):
+  number_upper = 0
+  number_lower = 0
+  for letter in text:
+    if letter.isupper():
+      number_upper += 1
+    elif letter.islower():
+      number_lower += 1
+  return "{} upper case letter and {} lower case letter".format(number_upper, number_lower)
+s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
+print upper_lower(s)
