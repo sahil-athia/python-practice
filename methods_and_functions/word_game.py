f3dbe8fffd384ea91668a8a8fba71de63fce7749
Sahil Athia <sahilathia918@gmail.com>
2021-03-13 20:16:09 -0500

    add third route

--- methods_and_functions/word_game.py
+++ methods_and_functions/word_game.py
@@ -8,12 +8,20 @@ def first_user_choice():
   return int(choice)
 
 def second_user_choice():
-  choice = "WRONG"
+  choice2 = "WRONG"
 
-  while choice.isdigit() == False:
-    choise = "{}".format(input("you are inside of the basement, do you enter 1: celler or 2: sneak out window: "))
+  while choice2.isdigit() == False:
+    choice2 = "{}".format(input("you are inside of the basement, do you enter 1: celler or 2: sneak out window: "))
   
-  return int(choice)
+  return int(choice2)
+
+def third_user_choice():
+  choice3 = "WRONG"
+
+  while choice3.isdigit() == False:
+    choice3 = "{}".format(input("you managed to escape into the backyard, do 1: run to the road or 2: call for help "))
+  
+  return int(choice3)
 
 answer1 = first_user_choice()
 
@@ -25,3 +33,10 @@ answer2 = second_user_choice()
 
 if answer2 == 2:
   print("a killer waited for you outside, you died")
+
+answer3 = third_user_choice()
+
+if answer3 == 2:
+  print("a killer managed to get you during the call, you died")
+
+
