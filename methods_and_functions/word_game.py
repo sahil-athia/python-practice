08d3903c0dcbcdb797a275f67f0a5cbcc5194974
Sahil Athia <sahilathia918@gmail.com>
2021-03-13 20:08:11 -0500

    add second route

--- methods_and_functions/word_game.py
+++ methods_and_functions/word_game.py
@@ -1,5 +1,5 @@
 
-def user_choice():
+def first_user_choice():
   choice = "WRONG"
 
   while choice.isdigit() == False:
@@ -7,9 +7,21 @@ def user_choice():
 
   return int(choice)
 
-answer1 = user_choice()
+def second_user_choice():
+  choice = "WRONG"
+
+  while choice.isdigit() == False:
+    choise = "{}".format(input("you are inside of the basement, do you enter 1: celler or 2: sneak out window: "))
+  
+  return int(choice)
+
+answer1 = first_user_choice()
 
 if answer1 == 1:
-  print("you chosse answer 1")
-elif answer1 == 2:
-  print("you chosse answer 2")
+  print("wrong door you died")
+
+
+answer2 = second_user_choice()
+
+if answer2 == 2:
+  print("a killer waited for you outside, you died")
