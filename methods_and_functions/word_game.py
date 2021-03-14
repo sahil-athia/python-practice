d03a4165f9866ca9d3fb1f83e274c54150f8de89
Sahil Athia <sahilathia918@gmail.com>
2021-03-13 20:01:39 -0500

    ensure input in number

--- methods_and_functions/word_game.py
+++ methods_and_functions/word_game.py
@@ -1,4 +1,13 @@
-answer1 = input("you are stuck in a house, which door would you like to take? 1 or 2: ")
+
+def user_choice():
+  choice = "WRONG"
+
+  while choice.isdigit() == False:
+    choice = "{}".format(input("you are stuck in a house, which door would you like to take? 1 or 2: "))
+
+  return int(choice)
+
+answer1 = user_choice()
 
 if answer1 == 1:
   print("you chosse answer 1")
