d3c4f6ab5b4d86ec2063ab3d0778439ace1d5a8b
Sahil Athia <sahilathia918@gmail.com>
2021-03-15 20:44:55 -0400

    add choice selector

--- milestone_project_1/user_interaction.py
+++ milestone_project_1/user_interaction.py
@@ -5,3 +5,16 @@ def display_game(game_list):
   print(game_list)
 
 display_game(game_list)
+
+def position_choice():
+  choice = "WRONG"
+
+  while choice not in ["0", "1", "2"]:
+    choice = input("pick a position (0, 1, 2): ")
+
+    if choice not in ["0", "1", "2"]:
+      print("invalid choice")
+    
+  return int(choice)
+
+position_choice()
