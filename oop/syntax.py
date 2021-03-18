3831f18dadc3d2c75722d6f75c7c1eb6a59c4c4d
Sahil Athia <sahilathia918@gmail.com>
2021-03-17 20:59:30 -0400

    add basic python class

---
+++ oop/syntax.py
@@ -0,0 +1,9 @@
+class nameOfClass():
+  def _init_(self, param1, param2):
+    self.param1 = param1
+    self.param2 = param2
+
+  def some_method(self):
+    # self word connects method to class
+    # do something 
+    print(self.param1)
