c48b53627e48c7629fce1f71ce9cad7c68f62cd3
Sahil Athia <sahilathia918@gmail.com>
2021-03-17 21:36:38 -0400

    add additional methods

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -6,5 +6,13 @@ class Animal():
 
   def __init__(self):
     print("ANIMAL CREATED")
+  
+  def who_am_i(self):
+    print("I am an animal")
+  
+  def eat(self):
+    print("I am eating")
 
 my_animal = Animal()
+my_animal.who_am_i()
+my_animal.eat()
