27611197f417f7b17ae7227e0f222e7368bc8f3e
Sahil Athia <sahilathia918@gmail.com>
2021-03-17 21:13:52 -0400

    add additional attributes

--- oop/class_and_attributes.py
+++ oop/class_and_attributes.py
@@ -1,8 +1,14 @@
 class Dog():
   
-  def __init__(self, breed):
+  def __init__(self, breed, name, spots):
     self.breed = breed
+    self.name = name
+
+    # expected boolean
+    self.spots = spots
 
 
-my_dog = Dog(breed = "lab")
+my_dog = Dog(breed = "lab", name="dawg", spots=False)
 print(my_dog.breed)
+print(my_dog.name)
+print(my_dog.spots)
