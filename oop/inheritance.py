50d0d24b0cfa379711c58ddd06e4a85b63124210
Sahil Athia <sahilathia918@gmail.com>
2021-03-17 21:38:50 -0400

    initialize the animal class on dog init

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -16,3 +16,11 @@ class Animal():
 my_animal = Animal()
 my_animal.who_am_i()
 my_animal.eat()
+
+# dogs are animals
+# dog is now a derrived class
+class Dog(Animal):
+
+  def __init__(self):
+    # create instance of animal class when we create an instance of the dog class
+    Animal.__init__(self)
