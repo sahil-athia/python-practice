e119c60dcdc3ecd8a46ddb99e74befaaebecd68f
Sahil Athia <sahilathia918@gmail.com>
2021-03-18 21:38:23 -0400

    add special str method

--- oop/special_methods.py
+++ oop/special_methods.py
@@ -6,4 +6,10 @@ class Book():
   def __init__(self, title, author, pages):
     self.title = title
     self.author = author
-    self.pages = pages
+    self.pages = pages 
+  
+  def __str__(self):
+    return f"{self.title} by {self.author}"
+
+b = Book("title", "person", 90)
+print(b)
